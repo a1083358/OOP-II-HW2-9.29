e638a55edd744a1377726d5a68f23c5bbb932af6
a1083358 <100353517+a1083358@users.noreply.github.com>
2022-11-12 20:16:24 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,33 @@
+//買票系統，有4台機器，隨機買1-4張票
 import java.security.SecureRandom;
 
 class device implements Runnable{
   Thread ticket;
   static int total=10000;
-  static int num=0;
-  int all_ticket=0;
-  public device(String name){
+  int num=0;//注意：一定要用 inner variable，不可以用static，否則機器1234可以取得對方的數字，num是每台機器都有屬於自己的變數
+  int all_ticket=0;//all_ticket是每台機器都有屬於自己的變數
+  public device(String name){//name為名字
     ticket=new Thread(this,name);
     ticket.start();
   }
   public void run(){
-    while(totalnumber()==true){
+    SecureRandom randomNumbers=new SecureRandom();
+    num=randomNumbers.nextInt(4)+1;
+    while(totalnumber(ticket.getName(),num)==true){
       all_ticket+=num;
       System.out.println(ticket.getName()+"賣出："+num+"張票");
+      num=randomNumbers.nextInt(4)+1;
     }
     System.out.println(ticket.getName()+"總共賣出："+all_ticket+"張票");
   }
-  synchronized static boolean totalnumber(){
-    SecureRandom randomNumbers=new SecureRandom();
+  private synchronized static boolean totalnumber(String name,int num){//確定有票可以買
     if(total>0){
-      num=randomNumbers.nextInt(4)+1;
-      total-=num;
-      return true;
+      if((total-num)<0){//注意:要先確認賣完票是否變負數的票
+        return false;
+      }else{//注意：應用if-else去寫，否則很可能即使return false了，但不知道為甚麼仍然會執行到return true，所以還是多一層保險比較好
+        total-=num;
+        return true;
+      }
     }else{
       return false;
     }
